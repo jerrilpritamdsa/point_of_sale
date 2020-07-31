f1852180cd0307127c04d5f6d2e9b3dde1210468
Jerril Pritam Dsa <jerrilpritamdsa@gmail.com>
2020-07-31 22:49:31 +0530

    Third commit

--- invoice/views.py
+++ invoice/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from pos.models import Product, Customer, Order, OrderItem
 import json
-
+import numpy as np
 def invoice_dashboard(request):
     return render(request, 'invoice_dashboard.html')
 
@@ -9,17 +9,26 @@ def customer_invoice(request):
     if request.method == 'POST':
         cid = request.POST.get('customerID', None)
         customer = Customer.objects.get(identity=cid)
-        print(customer)
+        
         customer_orders = Order.objects.filter(customer=cid).filter(success=True)
-
+        one=[]
+        two=[]
+        three=[]
+        for i in range(0,len(customer_orders)):
+            one.append(customer_orders[i])
+        for i in range(0,len(one)):
+            two.append(OrderItem.objects.filter(order_id=one[i]))
+        for each in range(0,len(two)):
+            for indi in range(0,len(two[each])):
+                three.append(two[each][indi].product.name)
         
         oitem=OrderItem.objects.filter(order_id=customer_orders[0])
         context = {'orders': [order for order in customer_orders],
                 'total': sum([int(order.total_price) for order in customer_orders]),
                 'customer': customer,
-                'oitem':oitem}
+                'three':three}
         
-        print([order for order in oitem])
+        
         return render(request, 'customer_invoice_detail.html', context)
     else:
         return render(request, 'customer_invoice.html')
